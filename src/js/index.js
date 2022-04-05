8d22af6187a3ac610f4a49b831fb9d889b5e5e20
Julia Prisniak <prisniak.j@gmail.com>
2022-04-05 14:32:21 +0200

    new

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 import '../css/styles.scss';
+
+const btn = document.querySelector('.prices__btn--name');
+const elem = document.querySelector('.prices__wrapper');
+
+btn.addEventListener('click', () => {
+    elem.classList.toggle('prices__wrapper--open')
+});
